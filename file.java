a430202e0c2bff2464193861c99edd7fb369bf28
Willy-557 <williamfsetiadi@gmail.com>
2025-12-23 12:07:01 +0700

    Make the menu of admin cinema

---
+++ file.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class file{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        ArrayList<String> daftarFilm = new ArrayList<>();
+
+        while (true) {
+            System.out.println("=== ADMIN BIOSKOP ===");
+            System.out.println("1. Tambah Film");
+            System.out.println("2. Hapus Film");
+            System.out.println("3. Lihat Daftar Film");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih menu: ");
+            int menu = scanner.nextInt();
+
+            
+        }
+    }
+}
